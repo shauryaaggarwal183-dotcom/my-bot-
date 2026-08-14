79184d78ad509628b9f74c2cf49a09d5041f1776
shauryaaggarwal183-dotcom <shauryaaggarwal183@gmail.com>
2026-08-14 17:02:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ class TicketBot(commands.Bot):
             "cogs.admin",
             "cogs.tier_test",
             "cogs.access",
-            "cogs.ai_qa",
             "cogs.comp",
         ]
 
